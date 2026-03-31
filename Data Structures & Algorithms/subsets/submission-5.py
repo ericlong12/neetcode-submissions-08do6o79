3000195f6b4442755e78047ce85c44b7991faeee
Eric Long <Ericsmainemail@gmail.com>
2026-03-31 09:32:53 -0800

    Add: subsets - submission-5

---
+++ Data Structures & Algorithms/subsets/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        answer = []
+        subset = []
+
+        def dfs(index):
+            if index == len(nums):
+                answer.append(subset.copy())
+                return
+            
+            # if we want to include nums[i]
+            subset.append(nums[index])
+            dfs(index+1)
+
+            # if we don't want to include nums[index]
+            subset.pop()
+            dfs(index+1)
+
+        dfs(0)
+        return answer
