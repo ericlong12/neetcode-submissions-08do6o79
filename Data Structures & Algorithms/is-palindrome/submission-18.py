e1acfc76b567d819e04db61f14a3fd68c9cd58e2
Eric Long <Ericsmainemail@gmail.com>
2026-03-30 23:00:09 -0800

    Add: is-palindrome - submission-18

---
+++ Data Structures & Algorithms/is-palindrome/submission-18.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # we have to ignore the cases
+        # we have to ignore the symbols
+
+        # we have function called lower
+
+        # char.isalnum
+        # lower()
+
+        # one pointer on left
+        # one pointer on right
+
+        # when pointers are equal to each other, we can exit loop
+
+        leftPointer = 0
+        rightPointer = len(s) - 1
+
+        while leftPointer <= rightPointer:
+            if s[leftPointer].isalnum():
+                if s[rightPointer].isalnum():
+                    if s[leftPointer].lower() == s[rightPointer].lower():
+                        leftPointer += 1
+                        rightPointer -= 1
+                    else:
+                        return False
+                else: 
+                    rightPointer -= 1
+            else:
+                leftPointer += 1
+        return True
