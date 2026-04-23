81a07e7584ea78383197ba123787015887c419e0
Eric Long <Ericsmainemail@gmail.com>
2026-04-23 08:03:08 -0800

    Add: three-integer-sum - submission-22

---
+++ Data Structures & Algorithms/three-integer-sum/submission-22.py
@@ -0,0 +1,62 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+        # summary of what I've learned
+        # use of continue -> its good if we want to skip something, kind of like a while loop break # skip one iteration
+        # use of break -> if we want to leave the whole loop. like a while loop break # this is to break out of the loop
+
+        # comparing duplicates make sure that we can compare with nums[index] with nums[index - 1]
+
+
+        # we start by soring the array nums
+
+        nums.sort()
+        # Input: nums = [-1,0,1,2,-1,-4]
+
+       #                        I.  L           R 
+        # sortedInput: nums = [-4, -1, -1, 0, 1, 2]
+        # lets go run index:
+        answer = []
+
+        for index in range(len(nums)):
+            leftPointer = index + 1
+            rightPointer = len(nums) - 1
+
+        
+
+            if nums[index] > 0:
+                break
+                # we exit the loop, bc we know that all the number to the right
+                # of index MUST be either equal or larger than nums[index]
+
+            if index > 0 and nums[index - 1] == nums[index]: # we could make this comparison, bc we know our index is at least 1 or greater
+                continue
+
+            if nums[index] <= 0:
+                while leftPointer < rightPointer:
+                    if nums[index] + nums[leftPointer] + nums[rightPointer] == 0:
+                        answer.append([nums[index], nums[leftPointer], nums[rightPointer]])
+                        leftPointer += 1 # we just added one here, so left pointer can never equal index.
+                        rightPointer -= 1
+
+                        while leftPointer < rightPointer and nums[leftPointer - 1] == nums[leftPointer]: # we could make this comparison because left pointer is ALWAYS to the right of index. gurantees us that there is a leftPointer - 1
+                            leftPointer += 1
+                    
+                    elif nums[index] + nums[leftPointer] + nums[rightPointer] < 0:
+                        leftPointer += 1
+                        
+                    elif nums[index] + nums[leftPointer] + nums[rightPointer] > 0:
+                        rightPointer -= 1
+        return answer
+
+
+
+
+
+
+
+
+
+
+            
+
